444db9f9c0f947da56ac15643181e726cb5d30f5
Rahman <museabdirahman@gmail.com>
2026-02-27 19:25:12 +0000

    Add Clerk authentication integration and update Navbar component
    - Wrapped the application in a ClerkProvider for authentication.
    - Enhanced Navbar to display user sign-in and sign-up buttons.
    - Updated package.json and package-lock.json to include @clerk/nextjs dependency.
    - Added proxy.ts for Clerk middleware configuration.

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -6,14 +6,27 @@ import React from "react";
 import KittDen from "@/public/logo.png";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 
 const navItems = [
   { id: 1, Label: "Library", href: "/" },
   { id: 2, Label: "Add New", href: "/books/new" },
 ];
 
+
+
 const Navbar = () => {
   const path = usePathname();
+
+  const {user } = useUser()
+
   return (
     <header className="w-full fixed bg(--bg-primary) z-40 ">
       <div className="wrapper py-4 navbar-height flex justify-between items-center">
@@ -27,7 +40,10 @@ const Navbar = () => {
               (item.href != "/" && path.startsWith(item.href));
             return (
               <Link
-                className={cn('nav-link-base', isActive? 'nav-link-active': 'text-black hover:opacity-70')}
+                className={cn(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
                 key={item.id}
                 href={item.href}
               >
@@ -35,6 +51,21 @@ const Navbar = () => {
               </Link>
             );
           })}
+          <div className="flex gap-7.5 items-center">
+            <SignedOut>
+              <SignInButton  mode="modal"/>
+            </SignedOut>
+            <SignedIn>
+              <div className="nav-user-link">
+                <UserButton />
+                {
+                  user?.firstName && (
+                    <Link href={'/subscriptions'} className="nav-user-name">{user.firstName}</Link>
+                  )
+                }
+              </div>
+            </SignedIn>
+          </div>
         </nav>
       </div>
     </header>
